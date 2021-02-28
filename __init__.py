ffa265c8846a794e6376180d989564080592c38f
zbenson94 <zackery@tyzame.com>
2021-02-28 13:25:38 -0500

    adding __init__.py

---
+++ __init__.py
@@ -0,0 +1,15 @@
+# ---------------------------------------------------------------------
+# AUTHOR: Zackery A. Benson
+# DATE:   28 February 2021
+# ---------------------------------------------------------------------
+
+
+# module for calculating collective motion
+# ---------------------------------------------------------------------
+import granular.affine
+
+# Formatting the data into pandas dataframes
+# ---------------------------------------------------------------------
+import granular.formatting
+
+
